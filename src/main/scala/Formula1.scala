81f25ccbc10581a589a9bb05d1f2a7c85a7f90f4
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2023-11-04 16:11:13 +0100

    Challenges 3 & 4

--- src/main/scala/Formula1.scala
+++ src/main/scala/Formula1.scala
@@ -7,6 +7,8 @@ object Formula1 {
     val challenges = new Formula1Challenges(spark)
     challenges.challengeOne()
     challenges.challengeTwo()
+    challenges.challengeThree()
+    challenges.challengeFour()
     spark.stop()
   }
 }
@@ -19,6 +21,7 @@ class Formula1Challenges(sparkSession: SparkSession) {
   private lazy val drivers = spark.read.format("csv").option("header", true).load("data/drivers.csv")
   private lazy val results = spark.read.format("csv").option("header", true).load("data/results.csv")
   private lazy val pitStops = spark.read.format("csv").option("header", true).load("data/pit_stops.csv")
+  private lazy val lapTimes = spark.read.format("csv").option("header", true).load("data/lap_times.csv")
 
   def challengeOne(): Unit = {
     val racesInMonaco = races
@@ -36,6 +39,18 @@ class Formula1Challenges(sparkSession: SparkSession) {
   }
 
   def challengeTwo(): Unit = {
+    val fastestLapDriversSum = results
+      .where(results("rank") === 1)
+      .groupBy(results("driverId"))
+      .agg(sum("rank") as "fastest_laps")
+      .join(drivers, "driverId", "inner")
+      .select("forename", "surname", "fastest_laps")
+      .orderBy(desc("fastest_laps"))
+
+    fastestLapDriversSum.write.format("csv").option("header", true).save("results/drivers-fastest-laps-sum")
+  }
+
+  def challengeThree(): Unit = {
     val racesIn2022 = races.filter(races("year") === 2022)
     val pitStopsRaces2022 = racesIn2022.join(pitStops, "raceId", "inner")
     val pitStopsPerDriver = pitStopsRaces2022
@@ -46,6 +61,29 @@ class Formula1Challenges(sparkSession: SparkSession) {
       .agg(avg("stops"))
 
     pitStopsAvgPerRace.write.format("csv").option("header", true).save("results/pit-stops-2022-avg")
+  }
+
+  def challengeFour(): Unit = {
+    lapTimes.createOrReplaceTempView("lap_times")
+    drivers.createOrReplaceTempView("drivers")
+
+    def millisecondsToHours(n: Long): Long = {
+      n / 3600000
+    }
+
+    val millisecondsToHoursUDF = udf(millisecondsToHours _)
+    spark.udf.register("msToHours", millisecondsToHoursUDF)
+
+    val driversTime = spark.sql(
+      """
+        |SELECT forename, surname, msToHours(sum(milliseconds)) AS time
+        |FROM lap_times
+        |JOIN drivers ON lap_times.driverId = drivers.driverId
+        |GROUP BY drivers.forename, drivers.surname
+        |ORDER BY time DESC
+        |""".stripMargin)
+
+    driversTime.write.format("csv").option("header", true).save("results/drivers-time")
   }
 }
       
